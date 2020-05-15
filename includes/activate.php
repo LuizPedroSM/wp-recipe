03aab6d371c3b54b645d548ac0818172fc3191b6
Luiz Pedro <luizpsmoreira@gmail.com>
2020-05-15 00:03:04 -0300

    add: create table

--- includes/activate.php
+++ includes/activate.php
@@ -5,4 +5,17 @@ function lr_activate_plugin()
     if (version_compare(get_bloginfo('version'), '4.5', '<')) {
         wp_die(__('Você precisa atualizar o WordPress para usar este plugin', 'recipes'));
     }
+
+    global $wpdb;
+
+    $sql = "CREATE TABLE ".$wpdb->prefix."recipes_votes(
+        ID BIGINT(20) NOT NULL AUTO_INCREMENT,
+        recipe_id BIGINT(20) NOT NULL,
+        vote TINYINT(1) NOT NULL,
+        user_ip VARCHAR(32) NOT NULL,
+        PRIMARY KEY (ID)
+    ) ".$wpdb->get_charset_collate();
+
+    require_once(ABSPATH.'/wp-admin/includes/upgrade.php');
+    dbDelta($sql);
 }
